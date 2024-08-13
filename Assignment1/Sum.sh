7cf520dd36b69c6191d5b3525fa8c2c4d716196a
Saptarshi Chakraborty <saptarshichakraborty04@outlook.com>
2024-08-13 21:13:37 +0530

    Assignment 1 updated

--- Assignment1/Sum.sh
+++ Assignment1/Sum.sh
@@ -1,11 +1,11 @@
 # !/bin/bash
 # write a bash script to take two numbers as input and print their sum
 
-echo "Hello world"
 echo "Enter first number"
 read num1
 echo "Enter second number"
 read num2
+
 sum=$((num1+num2))
 
-echo "Sum of $num1 and $num2 is $sum"
+echo "Sum of $num1 and $num2 is : $sum"
